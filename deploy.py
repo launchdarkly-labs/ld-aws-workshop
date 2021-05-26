ddbba4c22d686dae38848fb094513c39d6f4fa01
Lev Lazinskiy <lev@levlaz.org>
2021-05-25 17:16:55 -0700

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,1 +1,20 @@
-print('Hello World')
+import os
+import ldclient
+from ldclient.config import Config
+
+ldclient.set_config(Config(os.environ.get("LD_SDK_KEY")))
+ld_client = ldclient.get()
+
+tenants = ['ACME', 'BCDR', 'GFLM']
+    
+for tenant in tenants:
+    ctx = {
+    "key": tenant
+    }
+    
+    should_deploy = ld_client.variation("auto-deploy", ctx, False)
+    
+    if should_deploy:
+        print(f"DEPLOYING: {tenant}")
+    else:
+        print(f"NOT DEPLOYING: {tenant}")
